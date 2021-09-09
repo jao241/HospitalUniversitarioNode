49634543fb953bb91b03582f2fdb9dc6bfd6ac39
joao vitor <vorque45@gmail.com>
2021-09-09 17:11:40 -0300

    comentei o midleware de erro

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -11,16 +11,16 @@ const port = 3333;
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use((error:Error, request:Request, response:Response, next:NextFunction)=>{
-    if(error instanceof AppError){
-        return response.status(error.statusCode).json({
-            status: error.statusCode,
-            message: error.mensage
-        });
-    }
-    return response.status(500).json({
-        status: "Error.",
-        message: "Internal Server Error."
-    });
-})
+// app.use((error:Error, request:Request, response:Response, next:NextFunction)=>{
+//     if(error instanceof AppError){
+//         return response.status(error.statusCode).json({
+//             status: error.statusCode,
+//             message: error.mensage
+//         });
+//     }
+//     return response.status(500).json({
+//         status: "Error.",
+//         message: "Internal Server Error."
+//     });
+// })
 app.listen(port, ()=> console.log("Server on!"));
