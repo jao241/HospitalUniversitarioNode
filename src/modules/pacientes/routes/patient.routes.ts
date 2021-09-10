d5441914bb2f78dd6e3786ab7a032e40ab85c32d
João Vitor <vorque45@gmail.com>
2021-09-10 10:31:11 -0300

    alterei o nome da rota

--- src/modules/pacientes/routes/patient.routes.ts
+++ src/modules/pacientes/routes/patient.routes.ts
@@ -6,8 +6,8 @@ const patientController = new PatientController();
 patientRoutes.post("", patientController.create);
 patientRoutes.get("", patientController.index);
 patientRoutes.get("/:id", patientController.showById);
-patientRoutes.get("/:nome", patientController.showByNome);
-patientRoutes.get("/:cpf", patientController.showByCPF);
+patientRoutes.get("/nome/:nome", patientController.showByNome);
+patientRoutes.get("/cpf/:cpf", patientController.showByCPF);
 patientRoutes.put("/:id", patientController.update);
 patientRoutes.delete("/:id", patientController.delete);
 
