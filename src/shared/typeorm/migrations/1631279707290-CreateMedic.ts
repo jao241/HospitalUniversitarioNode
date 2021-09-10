07964505ea08ce15a6a9a78ad1147f0132c9d050
João Vitor <vorque45@gmail.com>
2021-09-10 10:32:34 -0300

    criei a migração para medicos

---
+++ src/shared/typeorm/migrations/1631279707290-CreateMedic.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateMedic1631279707290 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
