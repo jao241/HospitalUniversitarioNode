2fde4378aa3f43560fd05983bfc6608e9521bdf2
joao vitor <vorque45@gmail.com>
2021-09-13 17:15:58 -0300

    adicionei uma nova rota

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import doctorRoutes from "../../../modules/medicos/routes/doctor.routes";
 import patientRoutes from "../../../modules/pacientes/routes/patient.routes";
+import solicitacaoRoutes from "../../../modules/solicitacoes/routes/solicitacao.routes";
 
 const routes = Router();
 
 routes.use("/patients", patientRoutes);
 routes.use("/doctors", doctorRoutes);
+routes.use("/solicitacoes", solicitacaoRoutes);
 
 export default routes;
