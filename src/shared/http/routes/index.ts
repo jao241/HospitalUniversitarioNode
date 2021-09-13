b1d5f20e8f8fc329525b2590de2416470860c0cb
joao vitor <vorque45@gmail.com>
2021-09-13 15:10:54 -0300

    adicionei rota para médicos

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import doctorRoutes from "../../../modules/medicos/routes/doctor.routes";
 import patientRoutes from "../../../modules/pacientes/routes/patient.routes";
 
 const routes = Router();
 
 routes.use("/patients", patientRoutes);
+routes.use("/doctors", doctorRoutes);
 
 export default routes;
