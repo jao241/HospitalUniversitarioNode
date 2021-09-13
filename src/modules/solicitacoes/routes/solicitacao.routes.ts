afb20f7cd734ce02cee86b9bea69b4a0864c98e9
joao vitor <vorque45@gmail.com>
2021-09-13 17:15:45 -0300

    criei as rotas

---
+++ src/modules/solicitacoes/routes/solicitacao.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import SolicitacaoController from "../controllers/SolicitacaoController";
+
+const solicitacaoRoutes = Router();
+const solicitacaoController = new SolicitacaoController();
+
+solicitacaoRoutes.post("/", solicitacaoController.create);
+solicitacaoRoutes.get("/", solicitacaoController.index);
+solicitacaoRoutes.get("/:id", solicitacaoController.show);
+solicitacaoRoutes.put("/:id", solicitacaoController.update);
+solicitacaoRoutes.delete("/;id", solicitacaoController.delete);
+
+export default solicitacaoRoutes;
