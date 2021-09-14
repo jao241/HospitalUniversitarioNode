066f1ae08376f63679ac86eb9036a622b8d98427
joao vitor <vorque45@gmail.com>
2021-09-14 17:06:29 -0300

    adicionei a rota laudos

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import laudoRoutes from "../../../modules/laudos/routes/laudo.routes";
 import doctorRoutes from "../../../modules/medicos/routes/doctor.routes";
 import patientRoutes from "../../../modules/pacientes/routes/patient.routes";
 import solicitacaoRoutes from "../../../modules/solicitacoes/routes/solicitacao.routes";
@@ -8,5 +9,6 @@ const routes = Router();
 routes.use("/patients", patientRoutes);
 routes.use("/doctors", doctorRoutes);
 routes.use("/solicitacoes", solicitacaoRoutes);
+routes.use("/laudos", laudoRoutes);
 
 export default routes;
