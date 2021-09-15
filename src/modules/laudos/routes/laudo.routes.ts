0c2fbc282c38ca4c9435e5d52f557dc16dc4037f
joao vitor <vorque45@gmail.com>
2021-09-15 15:47:18 -0300

    adicionei mais rotas

--- src/modules/laudos/routes/laudo.routes.ts
+++ src/modules/laudos/routes/laudo.routes.ts
@@ -1,13 +1,55 @@
 import { Router } from "express";
 import LaudoController from "../controllers/LaudoController";
 import multer from "multer";
+import {celebrate, Segments} from "celebrate";
+import Joi from "joi";
 
 const laudoRoutes = Router();
 const laudoController = new LaudoController();
 const upload = multer({
-    storage: multer.memoryStorage()
+    dest: "uploads/"
 });
 
-laudoRoutes.post("/", upload.single("image_laudo"), laudoController.create);
-
+laudoRoutes.post("/", 
+upload.single("conclusao"), 
+celebrate({
+    [Segments.BODY]:{
+        descricao: Joi.string().required(),
+        conclusao: Joi.required(),
+        status: Joi.string().required(),
+        paciente_id: Joi.string().uuid().required(),
+        medico_id: Joi.string().uuid().required()
+    }
+})
+, laudoController.create);
+laudoRoutes.get("/", laudoController.index);
+laudoRoutes.get("/:id", 
+celebrate({
+    [Segments.PARAMS]:{
+        id: Joi.string().uuid().required()
+    }
+})
+, laudoController.show);
+laudoRoutes.put("/:id", 
+upload.single("conclusao"),
+celebrate({
+    [Segments.PARAMS]:{
+        id: Joi.string().uuid().required()
+    },
+    [Segments.BODY]:{
+        descricao: Joi.string().required(),
+        conclusao: Joi.required(),
+        status: Joi.string().required(),
+        paciente_id: Joi.string().uuid().required(),
+        medico_id: Joi.string().uuid().required()
+    }
+})
+, laudoController.update);
+laudoRoutes.delete("/:id",
+celebrate({
+    [Segments.PARAMS]:{
+        id: Joi.string().uuid().required()
+    }
+})
+, laudoController.delete);
 export default laudoRoutes;
