5f2d1950d1caccc6fd337ab5587044e6f95ea5ae
joao vitor <vorque45@gmail.com>
2021-09-27 17:18:41 -0300

    adicionei a rota para sessão

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import laudoRoutes from "../../../modules/laudos/routes/laudo.routes";
 import doctorRoutes from "../../../modules/medicos/routes/doctor.routes";
 import patientRoutes from "../../../modules/pacientes/routes/patient.routes";
+import sessionRoutes from "../../../modules/sessions/routes/SessionRoutes";
 import solicitacaoRoutes from "../../../modules/solicitacoes/routes/solicitacao.routes";
 
 const routes = Router();
@@ -10,5 +11,6 @@ routes.use("/patients", patientRoutes);
 routes.use("/doctors", doctorRoutes);
 routes.use("/solicitacoes", solicitacaoRoutes);
 routes.use("/laudos", laudoRoutes);
+routes.use("/sessions", sessionRoutes);
 
 export default routes;
