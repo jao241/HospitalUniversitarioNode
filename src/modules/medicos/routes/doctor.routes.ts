e45c307ef6a08ecad0e4a607debb9e55f9db3b9d
joao vitor <vorque45@gmail.com>
2021-09-27 16:19:47 -0300

    adicionei o celebrate as rotas

--- src/modules/medicos/routes/doctor.routes.ts
+++ src/modules/medicos/routes/doctor.routes.ts
@@ -1,16 +1,51 @@
 import { Router } from "express";
 import DoctorController from "../controllers/DoctorController";
+import { celebrate, Segments, Joi } from "celebrate";
 
 const doctorRoutes = Router();
 const doctorController = new DoctorController();
 
-doctorRoutes.post("/", doctorController.create);
+doctorRoutes.post("/", celebrate({
+    [Segments.BODY]:{
+        nome:Joi.string().required(),
+        crm:Joi.string().required(),
+        cpf:Joi.string().required(),
+        tipo:Joi.string().required(),
+        senha:Joi.string().required(),
+        ano_residencia:Joi.number().required(),
+        titulacao:Joi.string().required()
+    }
+}), doctorController.create);
 doctorRoutes.get("/", doctorController.index);
-doctorRoutes.get("/:id", doctorController.listOneDoctorById);
-doctorRoutes.get("/name/:nome", doctorController.listOneDoctorByName);
-doctorRoutes.get("/cpf/:cpf", doctorController.listOneDoctorByCPF);
-doctorRoutes.get("/crm/:crm", doctorController.listOneDoctorByCRM);
-doctorRoutes.put("/:id", doctorController.update);
-doctorRoutes.delete("/:id", doctorController.delete);
+doctorRoutes.get("/:id", celebrate({
+    [Segments.PARAMS]:{
+        id:Joi.string().uuid()
+    }
+}), doctorController.listOneDoctorById);
+doctorRoutes.get("/name/:nome", celebrate({
+    [Segments.PARAMS]:{
+        nome:Joi.string().required()
+    }
+}), doctorController.listOneDoctorByName);
+doctorRoutes.get("/cpf/:cpf", celebrate({
+    [Segments.PARAMS]:{
+        cpf:Joi.string().required()
+    }
+}), doctorController.listOneDoctorByCPF);
+doctorRoutes.get("/crm/:crm", celebrate({
+    [Segments.PARAMS]:{
+        crm:Joi.string().required()
+    }
+}), doctorController.listOneDoctorByCRM);
+doctorRoutes.put("/:id", celebrate({
+    [Segments.PARAMS]:{
+        id:Joi.string().uuid().required()
+    }
+}), doctorController.update);
+doctorRoutes.delete("/:id", celebrate({
+    [Segments.PARAMS]:{
+        id:Joi.string().uuid().required()
+    }
+}), doctorController.delete);
 
 export default doctorRoutes;
--- src/modules/pacientes/routes/patient.routes.ts
+++ src/modules/pacientes/routes/patient.routes.ts
@@ -1,14 +1,43 @@
 import { Router } from "express";
 import PatientController from "../controllers/PatientController";
+import { celebrate, Segments, Joi } from "celebrate";
 const patientRoutes = Router();
 const patientController = new PatientController();
 
-patientRoutes.post("", patientController.create);
+patientRoutes.post("", celebrate({
+    [Segments.BODY]:{
+        nome: Joi.string().required(),
+        genero:Joi.string().required(),
+        cor:Joi.string().required(),
+        cpf:Joi.string().required(),
+        data_nascimento:Joi.date().required()
+    }
+}), patientController.create);
 patientRoutes.get("", patientController.index);
-patientRoutes.get("/:id", patientController.showById);
-patientRoutes.get("/nome/:nome", patientController.showByNome);
-patientRoutes.get("/cpf/:cpf", patientController.showByCPF);
-patientRoutes.put("/:id", patientController.update);
-patientRoutes.delete("/:id", patientController.delete);
+patientRoutes.get("/:id", celebrate({
+    [Segments.PARAMS]:{
+        id:Joi.string().uuid().required()
+    }
+}), patientController.showById);
+patientRoutes.get("/nome/:nome", celebrate({
+    [Segments.PARAMS]:{
+        nome:Joi.string().required()
+    }
+}), patientController.showByNome);
+patientRoutes.get("/cpf/:cpf", celebrate({
+    [Segments.PARAMS]:{
+        cpf:Joi.string().required()
+    }
+}), patientController.showByCPF);
+patientRoutes.put("/:id", celebrate({
+    [Segments.PARAMS]:{
+        id:Joi.string().uuid().required()
+    }
+}), patientController.update);
+patientRoutes.delete("/:id", celebrate({
+    [Segments.PARAMS]:{
+        id:Joi.string().uuid().required()
+    }
+}), patientController.delete);
 
 export default patientRoutes;
