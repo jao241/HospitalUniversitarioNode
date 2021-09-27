fa8e7a8e41705eeddf352dc5b0759c413bccf385
joao vitor <vorque45@gmail.com>
2021-09-27 16:19:40 -0300

    adicionei o celebrate as rotas

--- src/modules/solicitacoes/routes/solicitacao.routes.ts
+++ src/modules/solicitacoes/routes/solicitacao.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import SolicitacaoController from "../controllers/SolicitacaoController";
-
+import { celebrate, Segments, Joi } from "celebrate";
 const solicitacaoRoutes = Router();
 const solicitacaoController = new SolicitacaoController();
 
-solicitacaoRoutes.post("/", solicitacaoController.create);
+solicitacaoRoutes.post("/", celebrate({
+    [Segments.BODY]:{
+        medico_crm:Joi.string().required()
+    }
+}), solicitacaoController.create);
 solicitacaoRoutes.get("/", solicitacaoController.index);
 solicitacaoRoutes.get("/:id", solicitacaoController.show);
 solicitacaoRoutes.put("/:id", solicitacaoController.update);
